cb9ad5b10ba29423344a787d813778c281794ca4
pouyanshams <148859501+pouyanshams@users.noreply.github.com>
2024-06-28 15:48:16 +0330

    Delete code-Q7 directory

--- code-Q7/work/@test@bench@processor/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity TestBenchProcessor is
-end TestBenchProcessor;
--- code-Q7/work/@vector@processor/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity VectorProcessor is
-    port(
-        opcode          : in     vl_logic_vector(1 downto 0);
-        rf_address      : in     vl_logic_vector(1 downto 0);
-        mem_address     : in     vl_logic_vector(4 downto 0)
-    );
-end VectorProcessor;
